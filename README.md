41e9108daf488df7d0512972a324f2d7b459c6ed
sakthivelan55 <sakthivel312002@gmail.com>
2021-10-14 19:43:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-h1vhkj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-h1vhkj)
---
+++ index.ts
@@ -0,0 +1,10 @@
+let today: Date = new Date();
+console.log(
+  'your data :',
+  today.getDate(),
+  today.getMonth(),
+  today.getFullYear(),
+  today.getHours(),
+  today.getMinutes(),
+  today.getSeconds()
+);
